6b3e49718484ac52bc19422df67a9ceaad38d2e4
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-01-31 12:54:03 -0300

    variables and selectors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-alert("hello word 2")
+var valor = document.querySelector("#exampleInputName")
+
+valor.value = "Joanete"
+valor.style.color = "purple"
+
+
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth")
+var country = document.querySelector("#exampleInputCountry")
+var email = document.querySelector("#exampleInputEmail1")
+var password = document.querySelector("#exampleInputPassword1")
+var photo = document.querySelector("#exampleInputFile")
+var admin = document.querySelector("#exampleInputAdmin")
