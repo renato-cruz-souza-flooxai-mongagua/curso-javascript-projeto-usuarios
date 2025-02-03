484815575548c95a0b1d1a5aa6894412fe040894
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-03 12:35:47 -0300

    feat: create user class to manage user data

---
+++ models.js/user.js
@@ -0,0 +1,13 @@
+class user {
+
+    constructor(name, gender, birth, coutry, email, password, photo,admin) {
+        this.name = name
+        this.gender = gender
+        this.birth = birth
+        this.coutry = coutry
+        this.email = email
+        this.password = password
+        this.photo = photo
+        this.admin = admin
+    }
+}
