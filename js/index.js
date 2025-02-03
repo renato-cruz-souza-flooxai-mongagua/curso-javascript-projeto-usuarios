4ba3568f3cf5c9a5284e9b5113c625b48258e471
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-03 10:31:10 -0300

    fix: form data capture

--- js/index.js
+++ js/index.js
@@ -13,3 +13,21 @@ fields.forEach(function(field, index){
 });
 
 console.log(user)
+
+// add clicks events
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+   event.preventDefault();
+   fields.forEach(function(field, index){
+
+    if (field.name == "gender") {
+        user.gender
+
+    } else {
+        user[field.name] = field.value
+    }
+
+});
+
+console.log(user)
+})
