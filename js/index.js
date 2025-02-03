fc5a08834b57295208a69db6f3581c6ee5b96682
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-03 09:58:35 -0300

    feat: add check for 'gender' field checked

--- js/index.js
+++ js/index.js
@@ -17,5 +17,10 @@ var fields = document.querySelectorAll("#form-user-create [name]")
 
 fields.forEach(function(field, index){
 
-    console.log(field.name)
+    if (field.name == "gender") {
+        if(field.checked === true) console.log("SIM", field)
+    } else {
+        console.log("NAO!")
+    }
+
 });
