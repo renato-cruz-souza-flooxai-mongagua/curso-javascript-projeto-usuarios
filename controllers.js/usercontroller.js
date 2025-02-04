7fcf222adcbd2a28f0d34e86c2366c2b1a75ca14
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-04 09:58:06 -0300

    refactor: getValues ​​method and add validation

--- controllers.js/usercontroller.js
+++ controllers.js/usercontroller.js
@@ -1,71 +1,53 @@
 class userController {
 
-
-    constructor(formId, tableId){
-        this.formEl = document.getElementById(formId)
-        this.tableEl = document.getElementById(tableId)
-        this.onSubmit()
+    constructor(formId, tableId) {
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+        this.onSubmit();
     }
 
     onSubmit() {
-
-
         this.formEl.addEventListener("submit", (event) => {
-
             event.preventDefault();
+            let userData = this.getValues();
 
-            this.addLine(this.getValues())
-
-        })
-      
+            if (userData) {
+                this.addLine(userData);
+            }
+        });
     }
 
     getValues() {
-
         let user = {};
 
-        this.formEl.elements
-
-        this.formEl.elements.forEach(function(field, index){
-
-            if (field.name == "gender") {
-                userData.gender
-        
-            } else {
-                userData [field.name] = field.value
+        [...this.formEl.elements].forEach((field) => {
+            if (field.name) {
+                if (field.name === "gender" && field.checked) {
+                    user.gender = field.value; 
+                } else if (field.type !== "radio") {
+                    user[field.name] = field.value;
+                }
             }
-        
         });
-        
-        return new user(user.name, 
-            user.gender, 
-            user.birth, 
-            user.country, 
-            user.email, 
-            user.password, 
-            user.photo, 
-            user.admin) 
 
-    } // fechamento 
-
-     addLine(dataUser){
-
-       
-       
-        this.tableEl.innerHTML = ` <tr>
-                        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                        <td>${dataUser.name}</td>
-                        <td>${dataUser.email}</td>
-                        <td>${dataUser.admin}</td>
-                        <td>${dataUser.birth}</td>
-                        <td>
-                          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                        </td>
-                      </tr>`
-        
-        document.getElementById(`table-users`)
+        return user; 
     }
-    
 
+    addLine(dataUser) {
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = `
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin ? "Sim" : "Não"}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        `;
+
+        this.tableEl.appendChild(tr); 
+    }
 }
